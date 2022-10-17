0eb75f2a49b746ae59ce02611310604245c4580f
zave <zave7@naver.com>
2022-10-17 23:55:25 +0900

    integer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    let _x = 1; // underscore 로 warning 을 무시할 수 있다.
+    let 한글 = "한글"; // 한글 변수도 가능해졌다.
+}
+
+fn integer() {
+    let unsigned: u8 = 123;
+    let unsigned16 :u16 = 123;
+    // 서로 다른 타입의 정수는 더할 수 없음
+    unsigned + unsigned16;
 }
