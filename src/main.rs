6e3516539691541c0f4188dd119096d7bd8b488a
zave <zave7@naver.com>
2022-10-18 00:27:20 +0900

    float

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,22 @@ fn main() {
     println!("Hello, world!");
     let _x = 1; // underscore 로 warning 을 무시할 수 있다.
     let 한글 = "한글"; // 한글 변수도 가능해졌다.
+    integer();
     char();
     casting();
+    float();
 }
 
 fn integer() {
+    println!("integer");
     let unsigned: u8 = 123;
     let unsigned16 :u16 = 123;
     // 서로 다른 타입의 정수는 더할 수 없음
     // 묵시적 타입 캐스팅이 되지 않는다.
     // unsigned + unsigned16;
+    let numberWithType = 0u8; // 숫자 끝에 타입 지정 가능
+    let numberWithUnderscore = 1___2___3___i32; // 숫자 사이 및 타입 사이 언더스코어 삽입 가능
+    println!("{} {}", numberWithType, numberWithUnderscore);
 }
 
 fn char() {
@@ -35,4 +41,12 @@ fn casting() {
     let char = '1' as u8;
     println!("casting char {}", char)
 
+}
+
+fn float() {
+    let my_number = 9.; // . 만 찍어도 float 타입이 됨
+    let other_number = 9;
+    // 서로 다른 타입 연산 불가능
+    // println!("{}", my_number + other_number);
+    println!("{}", my_number as i32 + other_number);
 }
